bbb3bd32483fb2ad34f86431b2b5190c18914d6d
Shazil <shazrk6@gmail.com>
2020-12-27 14:18:19 -0500

    Need to fix countdown buttons

--- app/src/main/java/com/hucorp/android/doccam/helper/CameraBar.java
+++ app/src/main/java/com/hucorp/android/doccam/helper/CameraBar.java
@@ -2,6 +2,7 @@ package com.hucorp.android.doccam.helper;
 
 import android.content.Context;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -14,6 +15,12 @@ import com.hucorp.android.doccam.interfaces.TimerToolbarCallbacks;
 
 public class CameraBar extends Toolbar implements View.OnClickListener, TimerToolbarCallbacks
 {
+    //Timer
+    boolean timeroptions = true;
+    boolean fivetimeron;
+    boolean tentimeron;
+    private Button mFiveTimerBtn;
+
     private CameraBarCallbacks mCallbacks;
 
     // Control layout elements
@@ -38,8 +45,15 @@ public class CameraBar extends Toolbar implements View.OnClickListener, TimerToo
     {
         if (mLayout != null) mLayout.setVisibility(View.GONE);
         mLayout = layout;
+        mFiveTimerBtn = (Button) mLayout.findViewById(R.id.fivetimer);
+
         mLayout.setVisibility(View.VISIBLE);
         this.setListeners();
+
+        //Countdown
+        ((Button) mLayout.findViewById(R.id.fivetimer)).setVisibility(View.GONE);
+        ((Button) mLayout.findViewById(R.id.tentimer)).setVisibility(View.GONE);
+
     }
 
     private void setListeners()
@@ -49,6 +63,11 @@ public class CameraBar extends Toolbar implements View.OnClickListener, TimerToo
             if (mLayout.getId() == R.id.default_camera_toolbar)
             {
                 ((ImageButton) mLayout.findViewById(R.id.action_settings)).setOnClickListener(this);
+
+                //Countdown timer
+                ((ImageButton) mLayout.findViewById(R.id.action_timer)).setOnClickListener(this);
+                ((Button) mLayout.findViewById(R.id.fivetimer)).setOnClickListener(this);
+                ((Button) mLayout.findViewById(R.id.tentimer)).setOnClickListener(this);
             }
         }
     }
@@ -76,11 +95,28 @@ public class CameraBar extends Toolbar implements View.OnClickListener, TimerToo
     public void onClick(View v)
     {
         ImageButton b = (ImageButton) v;
+        //Button t = (Button) v;
+
         if (mCallbacks != null)
         {
             if (b.getId() == R.id.action_settings)
             {
                 mCallbacks.onSettingsClick();
+            }
+
+            else if (b.getId() == R.id.action_timer)
+            {
+                if (timeroptions){
+                    ((Button) mLayout.findViewById(R.id.fivetimer)).setVisibility(View.VISIBLE);
+                    ((Button) mLayout.findViewById(R.id.tentimer)).setVisibility(View.VISIBLE);
+                    timeroptions = false;
+                }
+
+                else {
+                    ((Button) mLayout.findViewById(R.id.fivetimer)).setVisibility(View.GONE);
+                    ((Button) mLayout.findViewById(R.id.tentimer)).setVisibility(View.GONE);
+                    timeroptions = true;
+                }
             }
         }
     }
